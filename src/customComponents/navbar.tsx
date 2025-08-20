323fe03f3a1c5ac559739b520045dc706b79b561
Egeeoz <ozakdemirege@hotmail.com>
2025-08-20 20:06:51 +0200

    update navbar content

--- src/customComponents/navbar.tsx
+++ src/customComponents/navbar.tsx
@@ -10,16 +10,13 @@ import {
 const Navbar = () => {
   return (
     <nav className="grid grid-cols-[1fr_auto_1fr] border-b-2 border-b-[--foreground] py-3 justify-center items-center">
-      <h1 className="yellowtail-regular justify-self-start pl-10 text-3xl">
+      <h1 className="yellowtail-regular justify-self-start pl-10 text-4xl">
         Rifter
       </h1>
       <NavigationMenu viewport={false} className="dark">
         <NavigationMenuList>
           <NavigationMenuItem>
             <NavigationMenuLink>Comp generator</NavigationMenuLink>
-          </NavigationMenuItem>
-          <NavigationMenuItem>
-            <NavigationMenuLink>Create an account</NavigationMenuLink>
           </NavigationMenuItem>
           <NavigationMenuItem>
             <NavigationMenuTrigger className="font-normal">
@@ -38,6 +35,16 @@ const Navbar = () => {
                 </li>
               </ul>
             </NavigationMenuContent>
+          </NavigationMenuItem>
+        </NavigationMenuList>
+      </NavigationMenu>
+      <NavigationMenu viewport={false} className="dark justify-self-end pr-10">
+        <NavigationMenuList className="gap-1.5">
+          <NavigationMenuItem>
+            <NavigationMenuLink>Create an account</NavigationMenuLink>
+          </NavigationMenuItem>
+          <NavigationMenuItem>
+            <NavigationMenuLink>Login</NavigationMenuLink>
           </NavigationMenuItem>
         </NavigationMenuList>
       </NavigationMenu>
