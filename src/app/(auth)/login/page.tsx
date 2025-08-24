520bd4ebb47de7b53d45cca5e4e0ae5d57eff780
Egeeoz <ozakdemirege@hotmail.com>
2025-08-24 14:16:30 +0200

    Update back button styling, remove login with google button

--- src/app/(auth)/login/page.tsx
+++ src/app/(auth)/login/page.tsx
@@ -8,7 +8,7 @@ export default function Page() {
       <div className="w-full max-w-sm">
         <Link
           href="/"
-          className="pb-1 pl-1 underline text-base flex items-center gap-1"
+          className="pb-1 pl-1 underline text-base flex items-center gap-1 underline-offset-4"
         >
           <ArrowLeft size={18} />
           Back to home
