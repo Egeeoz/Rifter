749b59d6226e74e54b14a25ac1c6288bc4a3235c
Egeeoz <ozakdemirege@hotmail.com>
2025-12-11 15:15:22 +0100

    Build and optimize next.js config for deployement

--- next.config.ts
+++ next.config.ts
@@ -1,5 +1,9 @@
 import type { NextConfig } from 'next';
 
-const nextConfig: NextConfig = {};
+const nextConfig: NextConfig = {
+  output: 'export',
+  basePath: '/Rifter',
+  assetPrefix: '/Rifter/',
+};
 
 export default nextConfig;
